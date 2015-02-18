8fe52b793985ef88f0ee1b8b234cae112a57d1ea
nkgami <nkgami@ligure64.dip.jp>
2015-02-18 17:19:57 +0900

    change install.sh and remove .vim

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.vimrc .vimrc
-ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
