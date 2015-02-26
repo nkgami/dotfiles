8e11223a6a7ba214ce8582f807fc7e731ea17254
nkgami <nkgami@ligure64.dip.jp>
2015-02-26 20:30:20 +0900

    add .profile

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.Trash
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
